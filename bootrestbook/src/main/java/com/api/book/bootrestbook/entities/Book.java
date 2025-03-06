246b804ed05cdd30918ddecd1feb5c9063bd3ba8
PranavRM5 <pranavmahajan510@gmail.com>
2025-03-06 16:22:15 +0530

    Spring Boot

--- bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Book.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Book.java
@@ -11,13 +11,15 @@ public class Book {
     @Column(name = "bid")
     private int id;
     private String title;
-    private String author;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Author author;
 
 
     public Book(){
     }
 
-    public Book(int id, String title, String author) {
+    public Book(int id, String title, Author author) {
         this.id = id;
         this.title = title;
         this.author = author;
@@ -39,11 +41,11 @@ public class Book {
         this.title = title;
     }
 
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
 
